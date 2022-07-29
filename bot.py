bbe429293163c0fc0963316b91f151d8aac3ac90
Matheus Melo <matheus.melo.sf@gmail.com>
2022-07-29 01:34:02 -0300

    init Player instance when 1st interacting w/ bot

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import os
 from dotenv import load_dotenv
 from myView import myView
 from configManager import configManager
+from player import Player
 
 load_dotenv()
 
@@ -18,7 +19,7 @@ async def on_ready():
 
 
 @bot.event
-async def on_message(message):
+async def on_message(message: discord.Message):
 
     #* ignore messages sent from the bot itself, ignore channels that aren't the default
     if message.author == bot.user or message.channel.id != int(configManager().config['DEFAULT']['chat_id']):
@@ -26,6 +27,17 @@ async def on_message(message):
 
     if message.content == ('!bot'):
         # await message.channel.send(bot.user) #? task 4, respond message with discord name
+        author = message.author
+
+        player = Player(
+            discord_id = author.id,
+            name = author.name,
+            level = 1,
+            health = 250,
+            max_health = 250,
+            experience = 0,
+            silver = 0
+        )
         
         #* display myView when user types '!bot'
         await message.channel.send(view = myView(user=message.author))
